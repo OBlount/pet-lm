b758f5672b878cc4a89b4ecc819fa19fb5519d53
Owen Blount <oblount077@gmail.com>
2024-10-17 22:32:26 +0100

    began discoverability feature

---
+++ lib/discoverability.py
@@ -0,0 +1,119 @@
+# Document an employee
+def groomer_document_structuring(groomer):
+    return {
+            "introduction": [groomer["name"], groomer["role "], groomer["experience "]],
+            "specialisations": [groomer["specialisations"], groomer["favorite_breeds"]],
+            "services": groomer["pet_types"],
+            "personal_info": [groomer["hobbies"]],
+    }
+
+# Aggregate all the info we need for an employee biography
+def groomer_aggregation(structured_groomer):
+    aggregation = {}
+
+    aggregation["introduction"] = f"{structured_groomer['introduction'][0]}, a {structured_groomer['introduction'][1]} with {structured_groomer['introduction'][2]} years of experience, specialises in {structured_groomer['specialisations'][0]}."
+
+    pet_types = " and ".join(structured_groomer["services"])
+    aggregation["skills"] = f"they are skilled at grooming {pet_types}."
+
+    hobbies = ", ".join(structured_groomer["personal_info"][0][:-1]) + " and " + structured_groomer["personal_info"][0][-1]
+    aggregation["personal"] = f"they enjoy {hobbies}."
+
+    favourite_breeds = " and ".join(structured_groomer["specialisations"][1])
+    aggregation["breeds"] = f"{structured_groomer['introduction'][0]}'s favourite breeds are {favourite_breeds}."
+
+    return aggregation
+
+# Seemless connection between aggregated sentences
+def groomer_realisation(groomer):
+    intro = groomer["introduction"]
+    skills = "Note that " + groomer["skills"]
+    personal = "When away from grooming, " + groomer["personal"]
+    breeds = "Do you have a favourite breed? " + groomer["breeds"]
+    return f"{intro} {skills} {personal} {breeds}"
+
+# Print the entire employee list
+def groomer_test():
+    for helper in helpers:
+        structured = groomer_document_structuring(helper)
+        aggregated = groomer_aggregation (structured)
+        final_output = groomer_realisation(aggregated)
+        print(final_output,"\n")
+
+# Employees that the user can get to know more of
+helpers = [
+        {
+            "name": "Aisha",
+            "role ": "Pet Groomer",
+            "experience ": "5 years",
+            "expertise ": "dog grooming",
+            "hobbies": ["painting", "hiking"],
+            "specialisations": "poodle cuts",
+            "favorite_breeds": ["poodle", "golden retriever"],
+            "pet_types": ["dogs", "cats"]
+        },
+        {
+            "name": "Lila",
+            "role ": "Pet Groomer",
+            "experience ": "3 years",
+            "expertise ": "cat grooming",
+            "hobbies": ["reading", "cooking"],
+            "specialisations": "persian grooming",
+            "favorite_breeds": ["persian", "sphynx"],
+            "pet_types": ["cats", "rabbits"]
+        },
+        {
+            "name": "Mei-Ling",
+            "role ": "Pet Groomer",
+            "experience ": "7 years",
+            "expertise ": "all breeds grooming",
+            "hobbies": ["yoga", "traveling"],
+            "specialisations": "exotic breeds",
+            "favorite_breeds": ["bulldog", "beagle"],
+            "pet_types": ["dogs", "ferrets"]
+        },
+        {
+            "name": "Carlos",
+            "role ": "Pet Groomer",
+            "experience ": "4 years",
+            "expertise ": "senior pet grooming",
+            "hobbies": ["photography", "cycling"],
+            "specialisations": "senior dog grooming",
+            "favorite_breeds": ["chihuahua", "dachshund"],
+            "pet_types": ["dogs", "cats", "hamsters"]
+        }
+]
+
+# Help hints and chatbot function discoverability
+help_menu = [
+        {
+            "command": "greet",
+            "description": "Say hello to the bot.",
+            "lead": "I can greet"
+        },
+        {
+            "command": "help",
+            "description": "Learn more about the company and chatbot features",
+            "lead": "I can provide info"
+        },
+        {
+            "command": "pet care",
+            "description": "Get tips on how to take care of your pet.",
+            "lead": "I can assist in pet health and care"
+        },
+        {
+            "command": "grooming services",
+            "description": "Find out about the grooming services offered.",
+            "lead": "I can inform you about our pet grooming services"
+        },
+        {
+            "command": "book appointment",
+            "description": "Schedule an appointment for grooming.",
+            "lead": "I can book and appointment"
+        },
+        {
+            "command": "contact",
+            "description": "Get contact information for support.",
+            "lead": "I can connect you with our amazing staff"
+        }
+]
